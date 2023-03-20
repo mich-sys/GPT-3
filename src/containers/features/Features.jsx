5dfd8775904d6c6011a396ff430ae6f795f9b192
Ademeji Michael <85976163+mich-sys@users.noreply.github.com>
2023-03-20 04:04:52 +0000

    added new features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { Feature } from '../../components';
 import './features.css';
 
+const featuresData = [
+   {
+    title: 'Improving end distrusts Instantly',
+    text: 'From they fine John he gives instances to tons of people, which helps to Improve and end distrusts Instantly'
+   },
+   {
+    title: 'Become the tended active',
+    text: 'From they fine John he gives instances to tons of people, which helps to Improve and end distrusts Instantly'
+   },
+   {
+    title: 'Message or i am nothing',
+    text: 'From they fine John he gives instances to tons of people, which helps to Improve and end distrusts Instantly'
+   },
+   {
+    title: 'Really boy law county',
+    text: 'From they fine John he gives instances to tons of people, which helps to Improve and end distrusts Instantly'
+   }
+]
 const Features = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
       <div className="gpt3__features-heading">
         <h1 className="gradient__text">The Future Is Now And You Just Need To Realize It. Step Into Future Today & Make It Happen.</h1>
+        <p>Request Early Access To Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+         {featuresData.map((item, index) => (
+            <Feature title={item.title} text={item.text} key={item.title + index} />
+         ))}
       </div>
     </div>
   )
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import './possibility.css';
+import possibilityImage from '../../assets/Feature Image.png'
 
 const Possibility = () => {
   return (
-    <div>
-     Possibility
+    <div className="gpt3__possibility section__padding" id="possibility">
+     <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt='possibility' />
+     </div>
+     <div className="gpt3__possibility-content">
+       <h4>Request Early Access To Get Started</h4>
+       <h1 className="gradient__text">The Possibilities Are Beyond Your Imagination</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa.</p>
+       <h4>Request For Early Access</h4>
+     </div>
     </div>
-  )
+  );
 }
 
 export default Possibility 
